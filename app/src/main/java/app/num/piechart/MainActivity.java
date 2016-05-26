ed36ff42571bb137786eda622401b21e7cd2205e
Mukesh Jha <mukeshjha2007@gmail.com>
2016-05-26 11:47:45 +0530

    ist commt dyanmic bar and pie chart

--- app/src/main/java/app/num/piechart/MainActivity.java
+++ app/src/main/java/app/num/piechart/MainActivity.java
@@ -1,24 +1,145 @@
 package app.num.piechart;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayout mainLayout;
+    private PieChart pieChart;
+    private BarChart chart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PieChart pieChart = (PieChart) findViewById(R.id.chart);
+        mainLayout = (LinearLayout) findViewById(R.id.mainLayout);
+       // setPieChart();
+        setBarChart();
+    }
+
+    private void setBarChart() {
+        chart = new BarChart(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        chart.setLayoutParams(layoutParams);
+        mainLayout.addView(chart);
+        BarData data = new BarData(getXAxisValues(), getDataSet());
+        chart.setData(data);
+        chart.setDescription("My Chart");
+        chart.animateXY(2000, 2000);
+        chart.invalidate();
+    }
+
+    private ArrayList<BarDataSet> getDataSet() {
+        ArrayList<BarDataSet> dataSets = null;
+
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
+        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
+        valueSet1.add(v1e1);
+        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
+        valueSet1.add(v1e2);
+        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
+        valueSet1.add(v1e3);
+        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
+        valueSet1.add(v1e4);
+        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
+        valueSet1.add(v1e5);
+        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
+        valueSet1.add(v1e6);
+
+        ArrayList<BarEntry> valueSet2 = new ArrayList<>();
+        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
+        valueSet2.add(v2e1);
+        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
+        valueSet2.add(v2e2);
+        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
+        valueSet2.add(v2e3);
+        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
+        valueSet2.add(v2e4);
+        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
+        valueSet2.add(v2e5);
+        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
+        valueSet2.add(v2e6);
+
+
+        ArrayList<BarEntry> valueSet3 = new ArrayList<>();
+        BarEntry v2e13 = new BarEntry(10.000f, 0); // Jan
+        valueSet3.add(v2e13);
+        BarEntry v2e23 = new BarEntry(60.000f, 1); // Feb
+        valueSet3.add(v2e23);
+        BarEntry v2e33 = new BarEntry(10.000f, 2); // Mar
+        valueSet3.add(v2e33);
+        BarEntry v2e43 = new BarEntry(60.000f, 3); // Apr
+        valueSet3.add(v2e43);
+        BarEntry v2e53 = new BarEntry(45.000f, 4); // May
+        valueSet3.add(v2e53);
+        BarEntry v2e63 = new BarEntry(65.000f, 5); // Jun
+        valueSet3.add(v2e63);
+
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "INBOUND");
+        barDataSet1.setColor(Color.rgb(76, 153, 0));
+        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "OUTBOND");
+        barDataSet2.setColor(Color.rgb(0, 102, 204));
+        BarDataSet barDataSet3= new BarDataSet(valueSet3, "MISSED");
+        barDataSet3.setColor(Color.rgb(204, 0, 0));
+
+        dataSets = new ArrayList<>();
+        dataSets.add(barDataSet1);
+        dataSets.add(barDataSet2);
+        dataSets.add(barDataSet3);
+        return dataSets;
+    }
+
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> xAxis = new ArrayList<>();
+        xAxis.add("JAN");
+        xAxis.add("FEB");
+        xAxis.add("MAR");
+        xAxis.add("APR");
+        xAxis.add("MAY");
+        xAxis.add("JUN");
+        return xAxis;
+    }
+
+    private void setPieChart() {
+        pieChart = new PieChart(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        pieChart.setLayoutParams(layoutParams);
+        // add pie chart to main layout
+        mainLayout.addView(pieChart);
+
+
+        // PieChart pieChart = (PieChart) findViewById(R.id.chart);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColorTransparent(true);
+        pieChart.setHoleRadius(15);
+        pieChart.setTransparentCircleRadius(20);
+        pieChart.setUsePercentValues(true);
+        // customize legends
+        Legend l = pieChart.getLegend();
+        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+        l.setXEntrySpace(7);
+        l.setYEntrySpace(5);
+
 
         ArrayList<Entry> entries = new ArrayList<>();
         entries.add(new Entry(4f, 0));
@@ -29,6 +150,9 @@ public class MainActivity extends AppCompatActivity {
         entries.add(new Entry(9f, 5));
 
         PieDataSet dataset = new PieDataSet(entries, "# of Calls");
+        dataset.setSliceSpace(5);
+        dataset.setSelectionShift(7);
+
 
         ArrayList<String> labels = new ArrayList<String>();
         labels.add("January");
@@ -40,14 +164,15 @@ public class MainActivity extends AppCompatActivity {
 
         PieData data = new PieData(labels, dataset);
         dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(9f);
+        data.setValueTextColor(Color.BLACK);
         pieChart.setDescription("Description");
+
         pieChart.setData(data);
 
         pieChart.animateY(5000);
 
         pieChart.saveToGallery("/sd/mychart.jpg", 85); // 85 is the quality of the image
-
-
-
     }
 }
